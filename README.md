146994c8e364917bfc7ee2ed5b462540831713f3
Jannis Teunissen <jannis@teunissen.net>
2016-12-05 22:57:11 +0100

    Include optional category in config variables

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Suppose you want to use a grid of size `n_grid`, then you could do:
 
     integer     :: n_grid
     type(CFG_t) :: my_cfg
-    
+
     call CFG_add(my_cfg, "grid_size", 1024, "Size of the grid")
     call CFG_read_file(my_cfg, "my_input_file.txt")
     call CFG_get(my_cfg, "grid_size", n_grid)
@@ -34,13 +34,39 @@ for an more extensive example of the usage. If you have a sufficiently recent `g
 
 ## Configuration file syntax
 
-Lines with an `=`-sign should be of the form:
+There are three types of lines:
+
+1. Blank lines, or lines only containing a comment (`# ...`), which are ignored.
+2. Lines indicating the start of a category: `[category_name]`
+3. Lines with an `=`-sign. If they are part of a user-defined category, they
+   should start with an indent.
+
+An example of a configuration file is shown below
+
+    age = 29
+    name = John
+
+    [weather]
+        temperature = 25.2
+        humidity = 23.5
+
+    happy = .true.
+
+Note that `temperature` and `humidity` are indented, and that `happy` is not,
+which means that `happy` is not part of weather (it is in the default unnamed
+category). To place variables in a category, you add them like Fortran types:
+
+    call CFG_add(my_cfg, "weather%temperature", 25.0_dp, "The temperature")
+
+Variables can also be arrays:
 
     name_of_variable = value1 [value2 value3 ...] # Optional comment
 
-The extra values `[value2 value3 ...]` can be omitted for a scalar variable.
-Lines without an `=`-sign are simply ignored. A `#` indicates the start of a
-comment, which will be ignored.
+The extra values `[value2 value3 ...]` are omitted for a scalar variable. You
+can create variables of varying array size, by specifying `dynamic_size=.true.`
+when creating a config variable:
+
+    call CFG_add(my_cfg, "numbers", [1, 2], "Comment", dynamic_size=.true.)
 
 ## Requirements
 
--- test_m_config.f90
+++ test_m_config.f90
@@ -14,56 +14,67 @@ program test_m_config
 
   print *, "Testing m_config.f90 implementation"
 
-  ! Create some parameters
-  call CFG_add(my_cfg, "first_name", "jannis", &
-       "First name of the author of this code")
-  call CFG_add(my_cfg, "full_name", (/"Jannis   ", "Teunissen"/), &
-       "Full name of the author")
-  call CFG_add(my_cfg, "my_age", 25, &
-       "Age of the author of this code")
-  call CFG_add(my_cfg, "my_fav_reals", (/1.337_dp, 13.37_dp, 133.7_dp/), &
-       "My favorite numbers", dynamic_size=.true.)
-  call CFG_add(my_cfg, "lots_of_work", .true., &
-       "Whether I have a lot of work to do")
   call CFG_add(my_cfg, "filename", "this/is/a/filename", &
-       "Example of a filename")
+       "A string containing a filename")
+
+  ! Variables can be placed inside categories
+  call CFG_add(my_cfg, "author%age", 25, &
+       "Age of the author of this code")
+  call CFG_add(my_cfg, "author%fav_reals", (/1.337_dp, 13.37_dp, 133.7_dp/), &
+       "My favorite numbers", dynamic_size=.true.)
+  call CFG_add(my_cfg, "author%lots_of_work", .true., &
+       "Whether I have a lot of work to do")
+
+  ! Categories cannot be nested
+  call CFG_add(my_cfg, "author_name%first", "jannis", &
+       "First name of the author of this code")
+  call CFG_add(my_cfg, "author_name%full", &
+       ["Jannis   ", "Teunissen"], "Full name of the author")
+
+  call CFG_add(my_cfg, "weather%temperature", 25.0_dp, &
+       "Temperature (Celsius)")
+  call CFG_add(my_cfg, "weather%humidity", 90.0_dp, &
+       "Humidity (percent)")
 
   ! Sort the configuration (this can speed up looking for variables, but only if
   ! you have a sufficiently large number of them)
   call CFG_sort(my_cfg)
 
-  print *, "Original values"
+  print *, "**Original values**"
+  print *, ""
+
   ! Write to stdout (only when given the filename "stdout")
   call CFG_write(my_cfg, "stdout")
 
-  print *, "Reading in example_config.txt"
+  print *, "** Reading in example_config.text **"
   call CFG_read_file(my_cfg, "example_config.txt") ! Update values with file
 
-  print *, "Updated values: "
+  print *, "** Updated values: **"
+  print *, ""
   call CFG_write(my_cfg, "stdout")                 ! Write to stdout
   call CFG_write(my_cfg, "example_config_out.txt") ! Write to file
 
   print *, "The code below demonstrates how to get values: "
   print *, ""
 
-  call CFG_get(my_cfg, "lots_of_work", my_logic)
+  call CFG_get(my_cfg, "author%lots_of_work", my_logic)
   write(*, "(A25,L10)") "Lots of work: ", my_logic
 
-  call CFG_get(my_cfg, "my_age", my_int)
+  call CFG_get(my_cfg, "author%age", my_int)
   write(*, "(A25,I10)") "My age: ", my_int
 
-  call CFG_get_size(my_cfg, "my_fav_reals", n_reals)
-  write(*, "(A25,I10)") "# favourite numbers: ", n_reals
+  call CFG_get_size(my_cfg, "author%fav_reals", n_reals)
+  write(*, "(A25,I10)") "Size favourite numbers: ", n_reals
 
   ! Generate format string
   write(fmt_string, "(A,I0,A)") "(A25,", n_reals, "E10.2)"
 
   allocate(my_reals(n_reals))
-  call CFG_get(my_cfg, "my_fav_reals", my_reals)
+  call CFG_get(my_cfg, "author%fav_reals", my_reals)
   write(*, fmt_string) "Favourite numbers: ", my_reals
   deallocate(my_reals)
 
-  call CFG_get_type(my_cfg, "full_name", variable_type)
+  call CFG_get_type(my_cfg, "author_name%full", variable_type)
   write(*, "(A25,A10)") "Type of full name: ", CFG_type_names(variable_type)
 
 end program test_m_config
